0b7b3411d47ef251c6b39d507f9bbedd91b68320
Victor Cornille <victor.cornille@gmail.com>
2025-09-09 21:43:17 +0200

    feat: adding breakpoint and run handling to debug server

--- lib/debug.ml
+++ lib/debug.ml
@@ -7,6 +7,14 @@ type step = {
   mutable initial_step_sent: bool;
 }
 
+type breakpoints = {
+  mutable lines: (string * string) list;
+}
+
+let ok_status = 
+  let json = `Assoc [("status", `String "ok")] in
+  Yojson.Safe.to_string json
+
 let step_to_json step =
   let json = `Assoc [
     ("input", `String step.input);
@@ -29,14 +37,63 @@ let start_debug_mode (module Machine : Machine) (input : string) =
     head = head;
     initial_step_sent = false;
   } in
+  let breakpoints: breakpoints = {
+    lines = [];
+  } in
 
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
-    Dream.get "/next-step" (fun _ ->
+    Dream.get "/step" (fun _ ->
       let updated_input, state, head = Machine.compute_next_step current_step.input current_step.state current_step.head in
       current_step.input <- updated_input;
       current_step.state <- state;
       current_step.head <- head;
       Dream.json (step_to_json current_step));
+
+    Dream.get "/run" (fun _ ->
+      let rec run_until_breakpoint () =
+          if List.exists (fun (s) -> s = current_step.state) Machine.finals
+            || List.exists (fun (s, r) -> 
+            s = current_step.state
+            && r.[0] = current_step.input.[current_step.head]
+          ) breakpoints.lines
+          then ()
+          else
+            let updated_input, state, head = Machine.compute_next_step current_step.input current_step.state current_step.head in
+            current_step.input <- updated_input;
+            current_step.state <- state;
+            current_step.head <- head;
+            run_until_breakpoint ()
+      in
+      run_until_breakpoint ();
+      Dream.json (step_to_json current_step)
+    );
+
+    Dream.post "/breakpoint" (fun req ->
+      (* %lwt is a macro for handling Lwt promises *)
+      let%lwt body = Dream.body req in
+      match Yojson.Safe.from_string body with
+      | exception Yojson.Json_error msg ->
+          Dream.json (error_to_json msg)
+      | json ->
+          let open Yojson.Safe.Util in
+          let state = json |> member "state" |> to_string in
+          let read = json |> member "read" |> to_string in
+          breakpoints.lines <- (state, read) :: breakpoints.lines;
+          Dream.json (ok_status)
+    );
+
+    Dream.delete "/breakpoint" (fun req ->
+      let%lwt body = Dream.body req in
+      match Yojson.Safe.from_string body with
+      | exception Yojson.Json_error msg ->
+          Dream.json (error_to_json msg)
+      | json ->
+          let open Yojson.Safe.Util in
+          let state = json |> member "state" |> to_string in
+          let read = json |> member "read" |> to_string in
+          breakpoints.lines <- List.filter (fun (s, r) -> not (s = state && r = read)) breakpoints.lines;
+          Dream.json (ok_status)
+    );
   ]
